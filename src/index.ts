9641c1972b159fe4b633806519cb2874f11d7dff
Maurizio Crocci <launchsoft@kamae.pt>
2024-01-18 21:39:43 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,207 @@
-export const one = 1
-export const two = 2
+import { Workbook } from 'exceljs'
+import type { DataValidation, Style, Worksheet } from 'exceljs'
+
+type Example = string | Date | boolean | number
+export interface ColumnConfig {
+  key: string
+  prop?: string
+  columns?: ColumnConfig[]
+
+  style?: Style
+  width?: number
+  example?: Example
+  validation?: DataValidation
+}
+
+export function createWorkbook(config: ColumnConfig): Workbook {
+  const wb = new Workbook()
+  const ws = wb.addWorksheet()
+
+  const md = maxDepth(config)
+  const cols = getColumns(config)
+  padHeaders(cols, md)
+
+  const columns = cols.map((c) => {
+    const last = c[c.length - 1]
+
+    const style = c.find(cc => !!cc.style)?.style
+    const width = c.find(cc => !!cc.width)?.width
+
+    return {
+      header: [...c.map(cc => cc.key), exampleString(last.example)],
+      style,
+      width,
+      key: last.key,
+      validation: last.validation,
+    }
+  })
+
+  ws.columns = columns
+
+  // addValidations(ws, config, md);
+
+  return wb
+}
+
+export function read<T>(ws: Worksheet, config: ColumnConfig): T[] {
+  const md = maxDepth(config)
+  const leaves = getLeaves(config)
+
+  const data: T[] = []
+
+  ws.eachRow((r, rowNumber) => {
+    if (rowNumber <= md)
+      return
+
+    const t = {}
+
+    for (let i = 0; i < leaves.length; i++) {
+      const cell = r.getCell(i + 1)
+      const prop = leaves[i].prop
+
+      if (prop)
+        Reflect.set(t, prop, cell)
+    }
+
+    data.push(t as T)
+  })
+
+  return data
+}
+
+export function maxDepth(node: ColumnConfig): number {
+  return maxDepthTail(node, 0)
+}
+
+function maxDepthTail(node: ColumnConfig, acc: number): number {
+  if (!node.columns || node.columns.length === 0)
+    return acc
+
+  return Math.max(...node.columns.map(c => maxDepthTail(c, acc + 1)))
+}
+
+export function getColumns(node: ColumnConfig): ColumnConfig[][] {
+  return getColumnsTail(node, [])
+}
+
+function getColumnsTail(
+  node: ColumnConfig,
+  chain: ColumnConfig[],
+): ColumnConfig[][] {
+  if (!node.columns || node.columns.length === 0)
+    return [chain]
+
+  return node.columns.flatMap(c => getColumnsTail(c, [...chain, c]))
+}
+
+export function padHeaders(d: ColumnConfig[][], md: number) {
+  for (let i = 0; i < d.length; i++) {
+    const row = d[i]
+
+    if (row.length < md) {
+      const n = md - row.length
+
+      const newRow: ColumnConfig[] = []
+      for (let k = 0; k < n; k++) newRow.push({ key: '', prop: '' })
+
+      d[i] = [row[0], ...newRow, ...row.slice(1)]
+    }
+  }
+}
+
+export function exampleString(example?: Example): string {
+  if (typeof example === 'string')
+    return example
+
+  if (example instanceof Date)
+    return example.toISOString()
+
+  if (typeof example === 'boolean' || typeof example === 'number')
+    return `${example}`
+
+  return ''
+}
+
+export function getLeaves(node: ColumnConfig): ColumnConfig[] {
+  if (!node.columns || node.columns.length === 0)
+    return [node]
+
+  return node.columns.flatMap(c => getLeaves(c))
+}
+
+interface MergeInterval {
+  row: number
+  x: number
+  y: number
+}
+
+export function getMerges(columns: ColumnConfig[][]): MergeInterval[] {
+  const intervals: MergeInterval[] = []
+
+  let rows = []
+  for (let i = 0; i < columns.length; i++) {
+    const row = []
+
+    for (let j = 0; j < columns[i].length; j++)
+      row.push(columns[i][j].key)
+
+    rows.push(row)
+  }
+
+  rows = invertMatrix(rows)
+
+  for (let r = 0; r < rows.length; r++) {
+    const row = rows[r]
+
+    let start = 0
+    let end = 0
+
+    for (let i = 1; i <= row.length; i++) {
+      if (i === row.length || row[i] !== row[i - 1]) {
+        // End of current interval
+        end = i - 1
+
+        if (start !== end)
+          intervals.push({ row: r, x: start, y: end })
+
+        if (i < row.length) {
+          // Start a new interval
+          start = i
+          end = i
+        }
+      }
+    }
+  }
+
+  return intervals
+}
+
+export function invertMatrix(matrix: string[][]): string[][] {
+  const rows = matrix.length
+  const cols = matrix[0].length
+
+  const grid = []
+  for (let j = 0; j < cols; j++)
+    grid[j] = Array(rows)
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++)
+      grid[j][i] = matrix[i][j]
+  }
+  return grid
+}
+
+// function addValidations(ws: Worksheet, config: ColumnConfig, md: number) {
+//  const leaves = getLeaves(config);
+//
+//  for (let i = 0; i < leaves.length; i++) {
+//    const c = columnLetter(i);
+//
+//    if (leaves[i].validation)
+//      ws.dataValidations.add(`${c}${md}:${c}9999`, leaves[i].validation);
+//  }
+// }
+//
+// function columnLetter(i: number) {
+//  return "A" + i; // TODO:
+// }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,146 @@
 import { describe, expect, it } from 'vitest'
+import {
+  type ColumnConfig,
+  getColumns,
+  getLeaves,
+  getMerges,
+  invertMatrix,
+  maxDepth,
+  padHeaders,
+} from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+const exampleConfig: ColumnConfig = {
+  key: '',
+  columns: [
+    {
+      key: 'a',
+      columns: [{ key: 'aa' }, { key: 'ab' }],
+    },
+    {
+      key: 'b',
+      columns: [{ key: 'ba' }],
+    },
+    {
+      key: 'c',
+      columns: [{ key: 'ca' }, { key: 'cb' }],
+    },
+  ],
+}
+
+const deepExample: ColumnConfig = {
+  key: '',
+  columns: [
+    {
+      key: 'a',
+      columns: [{ key: 'aa' }, { key: 'ab' }],
+    },
+    {
+      key: 'b',
+      columns: [{ key: 'ba' }],
+    },
+    {
+      key: 'c',
+      columns: [
+        {
+          key: 'ca',
+          columns: [
+            {
+              key: 'caa',
+              columns: [{ key: 'caaa' }],
+            },
+          ],
+        },
+        { key: 'cb' },
+      ],
+    },
+  ],
+}
+
+describe('tree Operations', () => {
+  it('should find max depth', () => {
+    expect(maxDepth({ key: '' })).toBe(0)
+    expect(maxDepth(exampleConfig)).toBe(2)
+    expect(maxDepth(deepExample)).toBe(4)
+  })
+
+  it('keeps tree chain when flattening', () => {
+    const expected: ColumnConfig[][] = [
+      [{ key: 'a' }, { key: 'aa' }],
+      [{ key: 'a' }, { key: 'ab' }],
+      [{ key: 'b' }, { key: 'ba' }],
+      [{ key: 'c' }, { key: 'ca' }, { key: 'caa' }, { key: 'caaa' }],
+      [{ key: 'c' }, { key: 'cb' }],
+    ]
+
+    const actual = getColumns(deepExample)
+    const simplifiedActual = actual.map(r =>
+      r.map(rr => ({ key: rr.key })),
+    )
+    expect(simplifiedActual).toStrictEqual(expected)
+  })
+
+  it('leaves', () => {
+    const expected: ColumnConfig[] = [
+      { key: 'aa' },
+      { key: 'ab' },
+      { key: 'ba' },
+      { key: 'caaa' },
+      { key: 'cb' },
+    ]
+
+    const actual = getLeaves(deepExample)
+    const simplifiedActual = actual.map(rr => ({ key: rr.key }))
+
+    expect(simplifiedActual).toStrictEqual(expected)
+  })
+
+  it('merge intervals', () => {
+    const md = maxDepth(exampleConfig)
+    const cols = getColumns(exampleConfig)
+
+    padHeaders(cols, md)
+
+    const merges = getMerges(cols)
+    const expected = [
+      { row: 0, x: 0, y: 1 },
+      { row: 0, x: 3, y: 4 },
+    ]
+
+    expect(merges).toStrictEqual(expected)
+  })
+})
+
+describe('test matrix', () => {
+  it('invert square matrix', () => {
+    const input = [
+      ['1', '2', '3'],
+      ['4', '5', '6'],
+      ['7', '8', '9'],
+    ]
+
+    const expected = [
+      ['1', '4', '7'],
+      ['2', '5', '8'],
+      ['3', '6', '9'],
+    ]
+
+    expect(invertMatrix(input)).toStrictEqual(expected)
+  })
+
+  it('invert non-square matrix', () => {
+    const input = [
+      ['1', '2', '3', '4'],
+      ['5', '6', '7', '8'],
+      ['9', '10', '11', '12'],
+    ]
+
+    const expected = [
+      ['1', '5', '9'],
+      ['2', '6', '10'],
+      ['3', '7', '11'],
+      ['4', '8', '12'],
+    ]
+
+    expect(invertMatrix(input)).toStrictEqual(expected)
   })
 })
